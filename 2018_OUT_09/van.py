7bea3fe4a2aa00d383370e24041d115b79fe6bfa
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2018-10-09 19:09:06 -0300

    Projeto do dia 09 de outubro - v.4

--- 2018_OUT_09/van.py
+++ 2018_OUT_09/van.py
@@ -8,7 +8,15 @@ class Van:
         self.motorista = motorista
 
     def embarcar(self, Passageiro):
-        self.lista.append(Passageiro)
+        if len(self.lista) < 16:
+            self.lista.append(Passageiro)
+        else:
+            print('Van Lotada')
+
+    def listar_passageiros(self):
+        for item in self.lista:
+            print(item.nome)
+
 
 topic = Van('Orlando')
 topic.embarcar(Passageiro('Passageiro 1'))
@@ -18,3 +26,13 @@ topic.embarcar(Passageiro('Passageiro 4'))
 topic.embarcar(Passageiro('Passageiro 5'))
 topic.embarcar(Passageiro('Passageiro 6'))
 topic.embarcar(Passageiro('Passageiro 7'))
+topic.embarcar(Passageiro('Passageiro 8'))
+topic.embarcar(Passageiro('Passageiro 9'))
+topic.embarcar(Passageiro('Passageiro 10'))
+topic.embarcar(Passageiro('Passageiro 11'))
+topic.embarcar(Passageiro('Passageiro 12'))
+topic.embarcar(Passageiro('Passageiro 13'))
+topic.embarcar(Passageiro('Passageiro 14'))
+topic.embarcar(Passageiro('Passageiro 15'))
+topic.embarcar(Passageiro('Passageiro 16'))
+topic.embarcar(Passageiro('Passageiro 17'))
