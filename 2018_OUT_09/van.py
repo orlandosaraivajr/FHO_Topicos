ffca1a5ab4f59de0c1df62eae264b02d15c64065
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2018-10-09 18:46:49 -0300

    Projeto do dia 09 de outubro - v.2

--- 2018_OUT_09/van.py
+++ 2018_OUT_09/van.py
@@ -1,3 +1,11 @@
+class Passageiro:
+    def __init__(self, nome):
+        self.nome = nome
+
 class Van:
-	def __init__(self, motorista):
-		self.motorista = motorista
+    lista = []
+    def __init__(self, motorista=None):
+        self.motorista = motorista
+
+    def embarcar(self, Passageiro):
+        self.lista.append(Passageiro)
